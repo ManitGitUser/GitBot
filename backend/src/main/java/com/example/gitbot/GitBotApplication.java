23cc3725e8026c948e5500eeb5e6d66d7bf9cb4b
Manit <ManitHuMain@gmail.com>
2026-08-24 14:25:13 +0530

    structure

---
+++ backend/src/main/java/com/example/gitbot/GitBotApplication.java
@@ -0,0 +1,15 @@
+package com.example.gitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBotApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GitBotApplication.class, args);
+        //
+    }
+
+}
---
+++ backend/src/test/java/com/example/gitbot/GitBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
